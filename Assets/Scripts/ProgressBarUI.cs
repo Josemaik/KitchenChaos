0a66467d9bfa3f1865f37915383ac0a176903cfe
JosemasitoGamer <jose.soriano@live.u-tad.com>
2025-12-01 18:57:03 +0100

    Added Stove Counter to fried food

--- Assets/Scripts/ProgressBarUI.cs
+++ Assets/Scripts/ProgressBarUI.cs
@@ -6,20 +6,29 @@ using UnityEngine.UI;
 
 public class ProgressBarUI : MonoBehaviour
 {
-    [SerializeField] private CuttingCounter cuttingCounter;
+    [SerializeField] private GameObject hasProgressGameObject;
     [SerializeField] private Image barImage;
+
+    private IHasProgress hasProgress;
 
     private bool isFilling;
 
     private void Start()
     {
-        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+        hasProgress = hasProgressGameObject.GetComponent<IHasProgress>();
+        if(hasProgress == null)
+        {
+            Debug.LogError("IHasProgress component not found on hasProgressGameObject!", hasProgressGameObject);
+            return;
+        }
+
+        hasProgress.OnProgressChanged += HasProgress_OnProgressChanged;
         barImage.fillAmount = 0f;
         isFilling = false;
         Hide();
     }
 
-    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
+    private void HasProgress_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e)
     {
         if (!isFilling && e.progressNormalized >= 0f)
         {
